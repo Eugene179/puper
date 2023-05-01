7d54c5a647f84f021d6919a48bb719a1af8cbf72
Evgeny Shchegortsov <ESHCHegortsov@inno.tech>
2023-05-02 00:23:36 +0300

    The logic were duplicated

--- src/main/java/com/stepup/puper/MainApplication.java
+++ src/main/java/com/stepup/puper/MainApplication.java
@@ -3,5 +3,6 @@ package com.stepup.puper;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
